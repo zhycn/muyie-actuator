731deaa83e63bdf359fc2d5382ea102c67960c07
qizhaohong <qizhaohong_sh@qq.com>
2020-03-23 11:08:10 +0800

    Revert

--- src/main/java/org/muyie/actuator/Application.java
+++ src/main/java/org/muyie/actuator/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAdminServer
 public class Application {
 
   public static void main(String[] args) throws Exception {
